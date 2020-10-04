cb6d79096365aa358f124b6ccc82dd41dfe9c4c6
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-10-04 09:53:40 -0300

    fineshed quiz app

---
+++ src/Components/QuestionCard.tsx
@@ -0,0 +1,42 @@
+import { type } from 'os';
+import React from 'react';
+
+import { AnswerObject } from '../App';
+
+// styles
+import { ButtonWrapper, Wrapper } from './QuestionCard.styles';
+
+type Props = {
+    question: string;
+    answers: string[];
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: AnswerObject | undefined;
+    questionNr: number;
+    totalQuestion: number;
+}
+
+const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNr, totalQuestion }) => {
+    return (
+        <Wrapper>
+            <p className="number">
+                Question: {questionNr} / {totalQuestion}
+            </p>
+            <p dangerouslySetInnerHTML={{ __html: question }} />
+            <div>
+                {answers.map(answer => (
+                    <ButtonWrapper
+                        key={answer}
+                        correct={userAnswer?.correctAnswer === answer}
+                        userClicked={userAnswer?.answer === answer}
+                    >
+                        <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+                            <span dangerouslySetInnerHTML={{ __html: answer }} />
+                        </button>
+                    </ButtonWrapper>
+                ))}
+            </div>
+        </Wrapper>
+    )
+}
+
+export default QuestionCard;
